b62fdeb635d24750780a7bf903008a92fb80a8eb
Katie Surrence <katie@Katies-MacBook-Pro.local>
2022-12-27 14:35:24 -0300

    first commit

---
+++ denoise_and_run_ks.m
@@ -0,0 +1,17 @@
+dbstop if error
+
+addpath(genpath('.'));
+
+datadir = './155';
+fname = 'IG 155 - Recall - CtxtA - Safety.ns6.ns6';
+chanmapfile = 'Chan14.mat';
+
+NS6 = openNSx('read', fullfile(datadir, fname), 'uv');
+denoisedNS6 = NS6;
+denoisedNSG.Data = removeLineNoise_SpectrumEstimation(NS6.Data, 30000);
+
+fid = fopen(fullfile(datadir, 'data_binary.bin'), 'w');
+fwrite(fid, int16(denoisedNS6.Data), 'int16');
+fclose(fid);
+
+run_kilosort_onfile_ks(datadir, chanmapfile)
