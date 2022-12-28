04412e07035f7b5f6f57f5f7ea6202120a94d51c
Katie Surrence <katie@Katies-MacBook-Pro.local>
2022-12-28 11:27:46 -0300

    adapted for ks3

--- denoise_and_run_ks.m
+++ denoise_and_run_ks.m
@@ -4,7 +4,6 @@ addpath(genpath('C:\Users\Likhtik Lab\katie'));
 
 datadir = 'C:\Users\Likhtik Lab\katie\data\155';
 fname = 'IG 155 - Recall - CtxtA - Safety.ns6.ns6';
-chanmapfile = 'Chan14.mat';
 
 NS6 = openNSx('read', fullfile(datadir, fname), 'uv');
 denoisedNS6 = NS6;
@@ -14,4 +13,4 @@ fid = fopen(fullfile(datadir, 'data_binary.bin'), 'w');
 fwrite(fid, int16(denoisedNS6.Data), 'int16');
 fclose(fid);
 
-run_kilosort_onfile_ks(datadir, chanmapfile)
+main_kilosort3_ks(datadir);
