6cf2b428a23a689a69ccba68660a53baa25f16d1
Katie Surrence <Katie@Likhtik-Lab-GPU.bio.hunter.cuny.edu>
2023-05-04 14:42:53 -0400

    electrode changes to post processing
    Former-commit-id: b9d76f36dd914fab6da9a560c4bdeb88ab911e2a

--- phy_post_process.m
+++ phy_post_process.m
@@ -5,7 +5,7 @@ rmpath('C:\Users\Katie\likhtik_scripts\archive\')
 rmpath('C:\Users\Katie\software\archive\')
 
 
-mice = {'154',  '156', '158', '160', '161', '163', '175', '176', '177',...
+mice = {'154',  '156', '158', '160', '163',  '176', '177',...
     '178', '179', '180'};
 
 toneOffCode = 65535;
@@ -14,12 +14,12 @@ toneOnCode = 65502;
 clear data
 for i=1:length(mice)
    clear animal_data
-    mousedir = ['C:\Users\Katie\data\archive\Single_Cell_Data\IG' mice{i}];
+    mousedir = ['C:\Users\Katie\data\Single_Cell_Data\IG' mice{i}];
     animal_data.NS6 = openNSx('read', fullfile(mousedir, 'Safety.ns6'), 'uv');
     animal_data.NEV =  openNEV('read', fullfile(mousedir, 'Safety.nev'));
     save(['C:\Users\Katie\data\events\animal_data_' mice{i} '.mat'],  '-struct', 'animal_data', '-v7.3');
     if str2num(mice{i}) > 165; toneOnCode = 65436; end
-    data(i) = process_post_phy(mousedir, animal_data, toneOnCode, toneOffCode);
+    data(i) = process_post_phy_ks(mousedir, animal_data, toneOnCode, toneOffCode);
 end
 
 
